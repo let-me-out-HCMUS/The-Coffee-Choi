88c832fa89a0c8494b245b3058f4f957165b5d96
21120508 <ladykillal1232@gmail.com>
2023-12-08 11:13:32 +0700

    Add mongoDB connection

--- server/app.js
+++ server/app.js
@@ -3,5 +3,10 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+
+app.all("*", (req, res, next) => {
+  next(new Error(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log("DB connect succesfsful!");
+  });
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
