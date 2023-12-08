10ecbc6a71739726e0091626e3f5aa92a9967014
21120530 <phucmapcaumieu@gmail.com>
2023-12-08 09:51:59 +0700

    add tailwindcss

--- client/admin/src/App.jsx
+++ client/admin/src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
