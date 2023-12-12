0e65e420fd6cfcb91bfc71012fd72234bb282aa2
21120508 <ladykillal1232@gmail.com>
2023-12-12 09:13:30 +0700

    Add product model

---
+++ server/controllers/productController.js
@@ -0,0 +1,45 @@
+const Product = require("../models/Product");
+
+const catchAsync = require("../utils/catchAsync");
+exports.getAllProducts = catchAsync(async (req, res, next) => {
+  // TODO: add filter, sort, limit, pagination
+  const products = await Product.find();
+  res.status(200).json({
+    status: "success",
+    results: products.length,
+    data: {
+      products,
+    },
+  });
+});
+exports.getProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new AppError("No product found with that ID", 404));
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      product,
+    },
+  });
+});
+exports.deleteProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+  if (!product) {
+    return next(new AppError("No product found with that ID", 404));
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+exports.createProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      product,
+    },
+  });
+});
---
+++ server/models/Product.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    maxLength: [8, "Price cannot exceed 8 characters"],
+    default: 0,
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter product description"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+const Product = mongoose.model("Product", productSchema);
+export default Product;
