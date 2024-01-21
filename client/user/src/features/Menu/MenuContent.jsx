9afe3708a55a02b3d20584acfbc456bff6d2cbf1
21120262 <huukhangtc@gmail.com>
2024-01-21 16:58:07 +0700

    feat: [menu] show products by category

--- client/user/src/features/Menu/MenuContent.jsx
+++ client/user/src/features/Menu/MenuContent.jsx
@@ -1,16 +1,18 @@
 import ProductCard from "../ProductCard/ProductCard";
 
-export default function MenuContent({ category, products }) {
+export default function MenuContent({ categories, products }) {
   return (
     <div className=" mx-4 mt-16 lg:mt-0 lg:border-l-2 lg:border-solid lg:border-amber-800 lg:pl-10">
-      {category.map((item, index) => (
-        <div>
-          <div className=" mb-4 font-semibold text-2xl">{item.name}</div>
+      {categories.map((category) => (
+        <div key={category.id}>
+          <div className=" mb-4 font-semibold text-2xl">{category.name}</div>
 
           <div className=" mb-12 grid grid-cols-2 gap-x-8 md:grid-cols-4 md:gap-x-4 lg:grid-cols-4 lg:gap-x-4">
-            {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
+            {products
+              .filter((product) => product.category === category.id)
+              .map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
           </div>
         </div>
       ))}
--- client/user/src/pages/Menu.jsx
+++ client/user/src/pages/Menu.jsx
@@ -2,31 +2,47 @@ import MenuSide from "../features/Menu/MenuSide";
 import MenuContent from "../features/Menu/MenuContent";
 
 import productlist from '../mocks/ProductListHome/data';
+import { useState } from "react";
 
 export default function Menu() {
-  const category = [
+  const categorylist = [
     {
+      id: 'Coffee',
       name: "Cà phê",
       description: "Coffee blabla",
     },
     {
+      id: 'Tea',
       name: "Trà",
       description: "Tea blabla",
     },
     {
+      id: 'Dessert',
       name: "Đá xây",
       description: "Smoothie blabla",
     },
     {
+      id: 'Food',
       name: "Bánh",
       description: "Cookie blabla",
     },
   ];
 
+  const [selectedCategory, setSelectedCategory] = useState(categorylist);
+  
+  const onClickCategory = (category) => {
+    // console.log(category);
+    if (category === "All") {
+      setSelectedCategory(categorylist);
+      return;
+    }
+    setSelectedCategory(categorylist.filter((item) => item.id === category));
+  };
+
   return (
     <div className=" flex lg:my-28 lg:mx-16 ">
-      <MenuSide category={category} />
-      <MenuContent products={productlist} category={category} />
+      <MenuSide category={categorylist} onClickCategory={onClickCategory} selectedCategory={selectedCategory} />
+      <MenuContent products={productlist} categories={selectedCategory} />
     </div>
   );
 }
