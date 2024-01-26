be4e5a6c82ee447bd8852afc9acf067dc2b0d912
21120508 <ladykillal1232@gmail.com>
2024-01-26 15:19:38 +0700

    [server] add transaction https server

---
+++ server/transactionServer.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config({
+  path: "./config.env",
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log("DB transaction connect succesfsful!");
+  });
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "./configs/server.key")),
+  cert: fs.readFileSync(path.join(__dirname, "./configs/server.cert")),
+};
+
+// Create HTTPS server
+const server = https.createServer(options, app);
+const port = 8001;
+server.listen(port, () => {
+  console.log(`Transaction server listening on https://localhost:${port}`);
+});
