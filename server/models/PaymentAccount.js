e653c37b1e4f31d49993ce16d56617420c081ca9
21120508 <ladykillal1232@gmail.com>
2024-01-26 15:00:07 +0700

    [server] add paymentAccount and transaction schema

---
+++ server/models/PaymentAccount.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const paymentAccountSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: [true, "Payment account must belong to a user!"],
+  },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+  type: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "paypal",
+  },
+});
+
+const PaymentAccount = mongoose.model("PaymentAccount", paymentAccountSchema);
+
+module.exports = PaymentAccount;
