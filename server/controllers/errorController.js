ba2b43e3136957588063530cf3c7be45d9e8d66e
21120508 <ladykillal1232@gmail.com>
2024-01-26 07:39:36 +0700

    [server] install passport-google-oauth20

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -33,18 +33,38 @@ const handleJWTError = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Duplicate field value: ${Object.keys(
+    err.keyValue
+  )} = ${Object.values(err.keyValue)}`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
 const handleJWTExpiredError = (err) =>
   new AppError("Your token has expired!! Please login again", 401);
 
 module.exports = (err, req, res, next) => {
+  console.log(process.env.NODE_ENV);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    if (err.name === "CastError") err = handleCastErrorDB(err);
-    if (err.name === "JsonWebTokenError") err = handleJWTError(err);
-    if (err.name === "TokenExpiredError") err = handleJWTExpiredError(err);
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.name === "JsonWebTokenError") error = handleJWTError(error);
+    if (error.name === "TokenExpiredError")
+      error = handleJWTExpiredError(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
+mongoose.set("strictQuery", false);
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
