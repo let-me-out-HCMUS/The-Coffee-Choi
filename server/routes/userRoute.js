10cf0346ab2209df34ae4136d27eb0efe560e9a0
21120508 <ladykillal1232@gmail.com>
2024-01-26 14:37:36 +0700

    [server] add login with facebook

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -39,4 +39,26 @@ userRouter.get(
   }
 );
 
+userRouter.get(
+  "/auth/facebook",
+  passport.authenticate("facebook", {
+    scope: ["email"],
+    session: false,
+  })
+);
+
+userRouter.get(
+  "/auth/facebook/callback",
+  (req, res, next) => {
+    passport.authenticate("facebook", (err, profile) => {
+      req.user = profile;
+      next();
+    })(req, res, next);
+  },
+  (req, res) => {
+    const token = authController.signToken(req.user);
+    res.redirect(`http://localhost:3000/login-success/${token}`);
+  }
+);
+
 module.exports = userRouter;
