ec5281b0dab5b61c3663a487891f813f96980552
21120508 <ladykillal1232@gmail.com>
2024-01-27 21:47:44 +0700

    [server] add status category

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -6,7 +6,11 @@ const APIFeatures = require("../utils/apiFeatures");
 
 // Get all categories
 exports.getAllCategories = catchAsync(async (req, res, next) => {
-  const categories = await Category.find();
+  const feature = new APIFeatures(Category.find(), req.query)
+    .filter()
+    .sort()
+    .limit()
+    .paginate();
   res.status(200).json({
     status: "success",
     results: categories.length,
